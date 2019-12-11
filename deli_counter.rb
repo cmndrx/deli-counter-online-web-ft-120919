5fcb6b3c13fcd0c406d2a093bc1c3fac83189924
Autonyo Sanders <tonio.sanders@gmail.com>
2019-12-11 22:28:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.empty?
-    puts "There is nobody waiting to be sevred"
+    puts "There is nobody waiting to be sevred!"
   else puts ""
     katz_deli.shift
   end  
