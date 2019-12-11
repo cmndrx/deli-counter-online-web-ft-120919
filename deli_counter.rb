d04f9f55fb28aca5b05cb160bba5a8de5362532f
Autonyo Sanders <tonio.sanders@gmail.com>
2019-12-11 22:12:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   position = []
  
   if katz_deli.empty?
-    puts = ""
+    puts = "The line is currently empty."
   else num = 1
     katz_deli.each do |customer|
       position.push ""
